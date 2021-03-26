87c5efe4cecfe47fc9af64d548fe730a61fab664
namhyo01 <namhyo01@naver.com>
2021-03-27 08:02:01 +0900

    commit

--- Tokens.py
+++ Tokens.py
@@ -4,4 +4,22 @@ parser_symbols={',':'COMMA',';':'SEMI','+':'ADD','=':'Assign','-':'MINUS','*':'M
 # "" : STR, '' : CHR
 open_symbols={'{':'LB','(':'LPAREN','[':'LSB'}
 close_symbols={'}':'RB',')':'RPAREN',']':'RSB'}
+
+
 white_space = [' ','\t','\n']
+for i,character in enumerate(input_string):
+    
+    dfa.LoadTransitionTable(transitiontable[transition])    
+    nextState = dfa.PeekNextState(character)
+    if(nextState!="finish"):
+        dfa.SetState(nextState)
+    elif(nextState="change"):
+
+    else:
+        print("토큰이 {}로 분류되었습니다.".format(dfa.GetToken()))
+        dfa.Reset()
+        
+print("토큰이 {}로 분류되었습니다.".format(dfa.GetToken()))
+# dfa를 사용을 마쳤으면 종료
+dfa.Reset()
+print("-----------")
