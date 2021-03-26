d2461428c8a55e7d4ba929f9ee7cdbd0ef3864b6
namhyo01 <namhyo01@naver.com>
2021-03-27 02:04:03 +0900

    commit

---
+++ Lexical.py
@@ -0,0 +1,8 @@
+import sys
+symbols={'int':'INT','float':'FLOAT','char':'CHAR','string':'STRING','boolean':'BOOLEAN',',':'COMMA',';':'SEMI','if':'IF','else':'ELSE','while':'WHILE','class':'CLASS','return':'RETURN','+':'ADD','=':'Assign','-':'MINUS','*':'MUL','/':'DIV','<':'COMPARISON','>':'COMPARISON','==':'COMPARISON','!=':'COMPARISON','<=':'COMPARISON','>=':'COMPARISON','true':'TRUE','false':'FALSE'}
+# "" : STR, '' : CHR
+open_symbols={'{':'LB','(':'LPAREN','[':'LSB'}
+close_symbols={'}':'RB',')':'RPAREN',']':'RSB'}
+
+with open('a.txt','r') as f:
+    
