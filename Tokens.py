d202d5d64b14ac04eb68c675209489fccabaf70e
namhyo01 <namhyo01@naver.com>
2021-03-27 13:44:34 +0900

    dd

--- Tokens.py
+++ Tokens.py
@@ -1,4 +1,5 @@
 symbols={'int':'INT','char':'CHAR','string':'STRING','boolean':'BOOLEAN','true':'TRUE','false':'FALSE','if':'IF','else':'ELSE','while':'WHILE','class':'CLASS','return':'RETURN'}
+
 parser_symbols={',':'COMMA',';':'SEMI','+':'ADD','=':'Assign','-':'MINUS','*':'MUL','/':'DIV','<':'COMPARISON','>':'COMPARISON','==':'COMPARISON','!=':'COMPARISON','<=':'COMPARISON','>=':'COMPARISON'}
 
 # "" : STR, '' : CHR
