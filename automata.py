380601d003ea1bc41b6efc99b752491bc697d46d
namhyo01 <namhyo01@naver.com>
2021-04-08 19:56:50 +0900

    last commit

--- automata.py
+++ automata.py
@@ -9,7 +9,7 @@ class FiniteAutomaton:
         self.past = False # 이전에 처리하고 있던 문자들이 있었나 확인
         self.lexeme = "" # 문자들이 테이블에 해당되면 문자열로 엮어주기위해존재
         self.digitletters=False  # -구분용
-
+        self.check_digit_num = 0 
     def LoadTransitionTable(self, _dfa): # 트랜지션 테이블 생성
         self.table = _dfa["Table"]
         self.acceptedStates={}
@@ -29,18 +29,25 @@ class FiniteAutomaton:
             self.transition += 1 #다음 트랜지션 테이블을 찾으라고 1을 증가시키고
             return self.PeekNextState(_input,_dfa) #다음것을 찾아 떠난다
         if(_input not in self.table[self.currentState] and self.past==True):#만약에 해당 트랜지션테이블에 해당되는 것이 없는데 이전에 체크되던것이 있다. 예를들어 띄어쓰기를 안한 int a=2인 경우 a랑 =과 2를 다 따로 분리하기 위해서 만든 코드입니다
-            
+            if(self.check_digit_num==2):
+                #이떈 -가 INTEGER가 아닌 OPERATOR이어야한다
+                self.check_digit_num=0
+                self.transition=4
+                self.LoadTransitionTable(_dfa[self.transition])
+                self.currentState="T2"
             self.lexeme+=_input 
             return "need_continue" # 이부분은 현재 단어는 그 이전까지의 테이블트랜지션과는 다르지만 다른 트랜지션테이블에 맞는지 체크하기위해서 다시 돌려보기 위해 씁니다
         if ('-' == _input): # -처리를 하기 위해 존재한다.
             if(self.digitletters): # -는 만약에 앞에 숫자나 문자가 처리되고 있었다면 그것은 마이너스의 의미로 사용되어야한다.(Operator)
                 self.transition=4 # Operator Table번호
             else:
+                self.check_digit_num+=1
                 self.transition=1 # 아닌경우는 음수의 의미인 정수이다.
             self.LoadTransitionTable(_dfa[self.transition]) # 그 테이블을 로드한다.
 
         if _input in self.table[self.currentState]: # 만약 그 인풋이 그 테이블에 있다면
-            
+            if self.transition==1:
+                self.check_digit_num+=1
             nextState = self.table[self.currentState][_input] # 다음 스테이트는 그 현재상테에서 입력값이 가야하는 스테이트를 바라본다
             self.past=True # 그리고 이제 문자가 처리중이니 past는 True
             self.lexeme += _input # 문자열에 그 문자들을 집어넣는다
@@ -58,7 +65,7 @@ class FiniteAutomaton:
     
 
     def GetToken(self):
-        
+        self.check_digit_num=0
         if self.currentState in self.acceptedStates:
             if(self.acceptedStates[self.currentState]=="Integer" or self.acceptedStates[self.currentState]=="ID"):
                 self.digitletters=True
